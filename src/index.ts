b21e293b7bef18255f31701eefaaabffc889c834
Nikolas <niko.heidner@gmail.com>
2020-11-17 13:30:38 +0100

    fix

--- src/index.ts
+++ src/index.ts
@@ -14,8 +14,8 @@ app.get('/', (req: Request, res: Response) => {
 app.get('/auth', (req: Request, res: Response) => {
     res.redirect(
         `https://github.com/login/oauth/authorize?client_id=${
-            process.env.CLIENT_ID
-        }&redirect_uri=${process.env.YOUR_DOMAIN}/callback&scope=${
+            process.env.NIKOOAUTH_CLIENT_ID
+        }&redirect_uri=${process.env.NIKOOAUTH_YOUR_DOMAIN}/callback&scope=${
             req.query.scope || 'repo,user'
         }&state=${state}`
     );
@@ -30,13 +30,13 @@ app.get('/callback', async (req: Request, res: Response) => {
     let token;
     try {
         let accessTokenRes = await axios.post<string>(
-            `https://github.com/login/oauth/access_token?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&code=${req.query.code}&state=${state}`
+            `https://github.com/login/oauth/access_token?client_id=${process.env.NIKOOAUTH_CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&code=${req.query.code}&state=${state}`
         );
 
         let data = qs.parse(accessTokenRes.data);
         if (data.error) {
             res.status(500).send(
-                process.env.NODE_ENV !== 'production'
+                process.env.NIKOOAUTH_NODE_ENV !== 'production'
                     ? data.error
                     : 'Login failed'
             );
@@ -59,6 +59,6 @@ app.get('/callback', async (req: Request, res: Response) => {
     res.send(script);
 });
 
-console.log(`server running on ${process.env.PORT}`);
+console.log(`server running on ${process.env.NIKOOAUTH_PORT}`);
 
-app.listen(parseInt(process.env.PORT as string), '0.0.0.0');
+app.listen(parseInt(process.env.NIKOOAUTH_PORT as string));
