6fef4e0bd9c0a9342a23bc349e1fb4c44187853e
Nikolas <niko.heidner@gmail.com>
2020-11-25 14:10:34 +0100

    set cookie for state

--- src/index.ts
+++ src/index.ts
@@ -3,28 +3,40 @@ import crypto from 'crypto';
 import express, { Request, Response } from 'express';
 import axios from 'axios';
 import qs from 'querystring';
+import cookieParser from 'cookie-parser';
 
 const app = express();
+app.use(cookieParser(process.env.NIKOOAUTH_CLIENT_SECRET));
 
-const state = crypto.randomBytes(32).toString('hex');
+const cookieOptions = {
+    httpOnly: true,
+    signed: true,
+};
 
 app.get('/auth', (req: Request, res: Response) => {
+    const state = crypto.randomBytes(32).toString('hex');
+
     const redirectUrl = `https://github.com/login/oauth/authorize?client_id=${
         process.env.NIKOOAUTH_CLIENT_ID
     }&redirect_uri=${process.env.NIKOOAUTH_YOUR_DOMAIN}&scope=${
         req.query.scope || 'repo,user'
     }&state=${state}`;
+
+    res.cookie('state', state, {
+        ...cookieOptions,
+        maxAge: 1000 * 60 * 30,
+    });
     res.redirect(redirectUrl);
 });
 
 app.get('/', async (req: Request, res: Response) => {
-    if (req.query.state !== state) {
+    if (req.query.state !== req.signedCookies.state) {
         res.status(500).send('Login failed');
         return;
     }
 
     let token;
-    const accessTokenReqUrl = `https://github.com/login/oauth/access_token?client_id=${process.env.NIKOOAUTH_CLIENT_ID}&client_secret=${process.env.NIKOOAUTH_CLIENT_SECRET}&code=${req.query.code}&state=${state}`;
+    const accessTokenReqUrl = `https://github.com/login/oauth/access_token?client_id=${process.env.NIKOOAUTH_CLIENT_ID}&client_secret=${process.env.NIKOOAUTH_CLIENT_SECRET}&code=${req.query.code}&state=${req.signedCookies.state}`;
     try {
         let accessTokenRes = await axios.post<string>(accessTokenReqUrl);
 
@@ -63,7 +75,7 @@ app.get('/', async (req: Request, res: Response) => {
             window.opener.postMessage('authorizing:github', '*');
         })()
     </script>`;
-
+    res.clearCookie('state', cookieOptions);
     res.send(script);
 });
 
